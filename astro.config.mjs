e8d83e76717d901cb1aca949a6594721c5dd7711
Chrystian Calderon <chrystianralfc@gmail.com>
2026-02-13 15:28:43 -0400

    adding alias and i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,15 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url).pathname,
+      },
+    }
+  },
+  i18n: {
+    locales: ['en-us', 'es', 'fr'],
+    defaultLocale: 'en-us',
+    routing: 'manual',
   }
 });
